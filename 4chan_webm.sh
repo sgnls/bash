b2b0084b605ce32e33f246e91504cb04e0899361
sgnls <global@sgnls.net>
2016-06-12 00:27:52 +0100

    Commit

--- 4chan_webm.sh
+++ 4chan_webm.sh
@@ -19,7 +19,7 @@ mkdir -p $PWD/groups/$gn
 
 echo "Downloading files..."
 for i in $(cat $PWD/src/$gn | grep -oP '/gif/[^"]*.webm' | uniq);
-	do wget -q --show-progress -P $PWD/groups/$gn http://i.4cdn.org/$i;
+	do wget -q -nc --show-progress -P $PWD/groups/$gn http://i.4cdn.org/$i;
 done
 
 # !
